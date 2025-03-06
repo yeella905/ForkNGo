943aa548627f1e47dec4a6fe25f1c535b6b70bee
yeella905 <yeella905@gmail.com>
2025-03-06 15:37:44 -0500

    edit: added more componemts to the login.ejs file

--- server.js
+++ server.js
@@ -23,14 +23,14 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const widgetApiRoutes = require("./routes/widgets-api");
-const usersApiRoutes = require("./routes/users");
+const recipentsApiRoutes = require("./routes/recipents");
 const foodItemsApiRoutes = require("./routes/food_items");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use("/api/widgets", widgetApiRoutes);
-app.use("/api/users", usersApiRoutes);
+app.use("/api/recipents", recipentsApiRoutes);
 app.use("/api/food_items", foodItemsApiRoutes);
 // Note: mount other resources here, using the same pattern above
 
