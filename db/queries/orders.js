f3f8ffa41b7825ffa38efc85a74831faf3d38476
Soseh <sosehv@gmail.com>
2025-03-08 21:58:25 -0500

    Adds getOrders() to grab orders for a specific user from DB

--- db/queries/orders.js
+++ db/queries/orders.js
@@ -46,7 +46,7 @@ const createOrder = function(recipients_id, user_selected_pickup_time, estimated
           // Return the created order id along with its items
           return { orderId, cartItems };
         });
-      })
+    })
 
     .catch((err) => {
       console.log('Error adding the order', err);
@@ -54,4 +54,71 @@ const createOrder = function(recipients_id, user_selected_pickup_time, estimated
     });
 };
 
-module.exports = { createOrder };
+/**
+ * Get orders for a specific user
+ * @param {number} recipients_id The ID of the recipient
+ * @return {promise<Array>} A promise to the orders
+ */
+
+const getOrders = function(recipients_id) {
+  const queryString = `
+    SELECT
+      o.id as order_id,
+      o.user_selected_pickup_time,
+      o.estimated_pickup_time,
+      o.actual_pickup_time,
+      oi.id as order_item_id,
+      oi.quantity,
+      oi.price,
+      oi.tax,
+      oi.special_request,
+      f.id as food_item_id,
+      f.name as food_name,
+      f.image_url
+    FROM orders o
+    JOIN order_items oi ON o.id = oi.orders_id
+    JOIN food_items f ON oi.food_items_id = f.id
+    WHERE o.recipients_id = $1
+    ORDER BY o.id DESC, oi.id ASC;
+    `;
+
+  return db.query(queryString, [recipients_id])
+    .then(result => {
+      const orders = {};
+
+      // Group by order_id
+      result.rows.forEach(row => {
+        if (!orders[row.order_id]) {
+          orders[row.order_id] = {
+            id: row.order_id,
+            user_selected_pickup_time: row.estimated_pickup_time,
+            actual_pickup_time: row.actual_pickup_time,
+            items: []
+          };
+        }
+        orders[row.order_id].items.push({
+          id: row.order_item_id,
+          food_item_id: row.food_item_id,
+          name: row.food_name,
+          quantity: row.quantity,
+          price: row.price,
+          tax: row.tax,
+          special_request: row.special_request,
+          image_url: row.image_url
+        });
+      });
+
+      // Convert to array
+      return Object.values(orders);
+    })
+    .catch((err) => {
+      console.log('Error fetching orders', err);
+      throw err;
+    });
+};
+
+
+module.exports = {
+  createOrder,
+  getOrders
+};
