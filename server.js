689dc3c1b7b429c269aeba3fd717bbc35c568ba9
Soseh <sosehv@gmail.com>
2025-03-14 18:39:24 -0400

    Moves all routes except for home route to routes folder-in separate files

--- server.js
+++ server.js
@@ -5,13 +5,10 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const session = require("express-session"); //cookies parser
+const foodQueries = require("./db/queries/food_items");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-const foodQueries = require("./db/queries/food_items");
-const orderQueries = require("./db/queries/orders");
-const recipientQueries = require("./db/queries/recipients");
-const orders_admin = require("./db/queries/orders_admin");
 app.set("view engine", "ejs");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
@@ -31,23 +28,28 @@ app.use(
   })
 );
 
-// Separated Routes for each Resource
+// API routes
 const recipientsApiRoutes = require("./routes/recipients");
 const foodItemsApiRoutes = require("./routes/menu");
 const ordersApiRoutes = require("./routes/orders");
 
-// Mount all resource routes
-// Note: Endpoints that return data (eg. JSON) usually start with `/api`
+// Mount all API routes
 app.use("/api/recipients", recipientsApiRoutes);
 app.use("/api/food_items", foodItemsApiRoutes);
 app.use("/api/orders", ordersApiRoutes);
-// Note: mount other resources here, using the same pattern above
+
+// Page rendering routes
+const viewPagesRoutes = require("./routes/view-pages");
+const authRoutes = require("./routes/auth-routes");
+const adminRoutes = require("./routes/admin-routes");
+
+// Mount all page rendering routes
+app.use("/", viewPagesRoutes);
+app.use("/", authRoutes);
+app.use("/admin", adminRoutes);
+
 
 // Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-
-// Fetch food items and render them on the home page
 app.get("/", (req, res) => {
   const user = req.session.user || null;
   console.log(">>>> SESSION");
@@ -63,95 +65,6 @@ app.get("/", (req, res) => {
     });
 });
 
-app.get('/orders', (req, res) => {
-  const user = req.session.user;
-
-  // If user is not logged in just show the cart
-  if (!user) {
-    return res.render('orders', { user: null, orders: [] });
-  }
-
-  // Get user's orders
-  orderQueries.getOrders(user.id)
-    .then(orders => {
-      res.render('orders', { user, orders });
-    })
-    .catch(err => {
-      console.error('Error fetching orders:', err);
-      res.render('orders', { user, orders: [], error: 'Error fetching orders'});
-    });
-});
-
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-
-app.post("/login", async(req, res) => {
-  // Fetch user from the database
-  recipientQueries
-    .getUser(req.body)
-    .then((user) => {
-      if (user.length === 0) {
-        return res.status(401).send("error email or username not found"); //if user provides wrong email or name, display 401 error message
-      }
-      req.session.user = { name: user.name, is_admin: user.is_admin, recipient_id: user.id }; //once the user logs in, the session will belong to that user. this is how we are going to access the user in other ejs files
-      if (user.is_admin) {
-        res.redirect("/admin");
-      } else {
-        res.redirect("/");
-      }
-    })
-    .catch((e) => res.send(e));
-});
-
-app.get("/logout", (req, res) => {
-  //when user logs out, destroy session and remove user data
-  req.session.destroy((error) => {
-    if (error) {
-      res.status(500).send("failed to logout");
-    }
-  });
-  res.redirect("/");
-});
-
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
-
-app.get("/admin", (req, res) => {
-  const user = req.session.user || null;
-  if (user.is_admin) {
-    orders_admin
-      .getAdminOrders()
-      .then((ordersAdmin) => {
-        console.log(ordersAdmin);
-        res.render("admin", { ordersAdmin, user });
-      })
-      .catch((err) => {
-        console.error("Error fetching order:", err);
-        res.status(500).send("Error fetching order");
-      });
-  } else {
-    res.redirect("/");
-  }
-});
-
-app.post("/update-status", (req, res) => {
-  const { orderId, newStatus } = req.body;
-
-  if (!orderId || !newStatus) {
-    //making sure orderId and newStatus is passed properly
-    return res.status(400).send("Missing orderId or newStatus");
-  }
-
-  orders_admin
-    .updateOrder(orderId, newStatus)
-    .then((response) => {
-      console.log("Update response:", response);
-      res.json({ response });
-    })
-    .catch((err) => {
-      console.error("Error updating order:", err);
-      res.status(500).send("Error updating order");
-    });
-});
+  console.log(`ForkNGo app listening on port ${PORT}`);
+})
