a9ae99884e9473c69ce9590a5935d58b9b8b0308
Soseh <sosehv@gmail.com>
2025-03-14 13:15:18 -0400

    Removes getOrderById()

--- db/queries/orders_admin.js
+++ db/queries/orders_admin.js
@@ -27,33 +27,6 @@ const getAdminOrders = () => {
     });
 };
 
-// Get a specific order by ID
-const getOrderById = (orderId) => {
-  return db
-    .query(
-      `SELECT orders.*, recipients.name, recipients.phone,
-      array_agg(
-      DISTINCT jsonb_build_object(
-      'order_item_id', order_items.id,
-      'food_item_id', order_items.food_items_id,
-      'price', order_items.price,
-      'qty', order_items.quantity,
-      'tax', order_items.tax,
-      'special_request', order_items.special_request,
-      'food_name', food_items.name)) AS items
-      FROM orders
-      INNER JOIN recipients ON orders.recipients_id = recipients.id
-      INNER JOIN order_items ON orders.id = order_items.orders_id
-      INNER JOIN food_items ON order_items.food_items_id = food_items.id
-      WHERE orders.id = $1
-      GROUP BY orders.id, recipients.id;`,
-      [orderId]
-    )
-    .then((data) => {
-      return data.rows[0];
-    });
-};
-
 const updateOrder = (orderId, status) => {
   const querystring = `UPDATE orders SET order_status = $2
       WHERE id = $1`;
@@ -69,4 +42,4 @@ const updateOrder = (orderId, status) => {
     });
 };
 
-module.exports = { getAdminOrders, updateOrder, getOrderById };
+module.exports = { getAdminOrders, updateOrder };
