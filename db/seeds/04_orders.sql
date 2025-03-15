a4ca46dcfe00607ccd5d0fc918946b51575964f3
Soseh <sosehv@gmail.com>
2025-03-15 15:40:28 -0400

    Fixes misspelling of the word 'received'

--- db/seeds/04_orders.sql
+++ db/seeds/04_orders.sql
@@ -1,5 +1,5 @@
 -- Seed for orders
 INSERT INTO orders (recipients_id, order_status)
 VALUES
-  (1, 'recieved'),  -- Customer 1's order
+  (1, 'received'),  -- Customer 1's order
   (2, 'in progress');  -- Customer 2's order
